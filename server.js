0a8db9b0d299491942ad6e502be1cb5cc5295a82
Priyanka Shahasane <priyankashahasane@Priyankas-MacBook-Air.local>
2024-07-15 01:57:00 +0530

    80 to 8080

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 // MongoDB connection
